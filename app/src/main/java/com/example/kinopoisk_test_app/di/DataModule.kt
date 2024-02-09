5ccbeff6ddac18b32e0d05104bb5e7e80e33e23e
Mark Quarasique <julipaha@gmail.com>
2024-02-10 01:31:53 +0300

    Добавляет инициализацию retrofit в kion модуль

--- app/src/main/java/com/example/kinopoisk_test_app/di/DataModule.kt
+++ app/src/main/java/com/example/kinopoisk_test_app/di/DataModule.kt
@@ -1,7 +1,17 @@
 package com.example.kinopoisk_test_app.di
 
+import com.example.kinopoisk_test_app.data.network.api.KinopoiskApi
+import com.example.kinopoisk_test_app.util.BASE_URL
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val dataModule = module {
-
+    single<KinopoiskApi> {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(KinopoiskApi::class.java)
+    }
 }
