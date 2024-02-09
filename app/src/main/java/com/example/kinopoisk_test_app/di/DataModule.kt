80b13423ddc6dd1f1967f1f43f76626b46efde1e
Mark Quarasique <julipaha@gmail.com>
2024-02-10 01:34:53 +0300

    Добавляет gson в di

--- app/src/main/java/com/example/kinopoisk_test_app/di/DataModule.kt
+++ app/src/main/java/com/example/kinopoisk_test_app/di/DataModule.kt
@@ -1,7 +1,11 @@
 package com.example.kinopoisk_test_app.di
 
+import com.example.kinopoisk_test_app.data.NetworkClient
 import com.example.kinopoisk_test_app.data.network.api.KinopoiskApi
+import com.example.kinopoisk_test_app.data.network.impl.RetrofitNetworkClient
 import com.example.kinopoisk_test_app.util.BASE_URL
+import com.google.gson.Gson
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,4 +18,10 @@ val dataModule = module {
             .build()
             .create(KinopoiskApi::class.java)
     }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(kinopoiskApi = get(), context = androidContext())
+    }
+
+    factory { Gson() }
 }
