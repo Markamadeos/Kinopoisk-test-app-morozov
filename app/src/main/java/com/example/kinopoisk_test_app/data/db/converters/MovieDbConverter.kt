29934f2bb27035b2e70901d39c69bcc34d12bc63
Mark Quarasique <julipaha@gmail.com>
2024-02-10 01:57:10 +0300

    Добавляет конвертер моделей для бд

---
+++ app/src/main/java/com/example/kinopoisk_test_app/data/db/converters/MovieDbConverter.kt
@@ -0,0 +1,32 @@
+package com.example.kinopoisk_test_app.data.db.converters
+
+import com.example.kinopoisk_test_app.data.db.entity.MovieEntity
+import com.example.kinopoisk_test_app.domian.models.Movie
+
+class MovieDbConverter {
+    fun map(movie: Movie): MovieEntity {
+        return MovieEntity(
+            id = movie.id,
+            name = movie.name,
+            cover = movie.cover,
+            coverSmall = movie.coverSmall,
+            year = movie.year,
+            description = movie.description,
+            countries = movie.countries,
+            genres = movie.genres
+        )
+    }
+
+    fun map(movie: MovieEntity): Movie {
+        return Movie(
+            id = movie.id,
+            name = movie.name,
+            cover = movie.cover,
+            coverSmall = movie.coverSmall,
+            year = movie.year,
+            description = movie.description,
+            countries = movie.countries,
+            genres = movie.genres
+        )
+    }
+}
