c9642ac15c95d12e338dce86b928339ec5367129
Mark Quarasique <julipaha@gmail.com>
2024-02-09 19:39:52 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kinopoisk-test-app"
+include(":app")
