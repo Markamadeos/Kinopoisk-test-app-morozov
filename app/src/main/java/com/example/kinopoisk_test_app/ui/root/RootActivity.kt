00cc7ac61095c356452217ec70342320b397abba
Mark Quarasique <julipaha@gmail.com>
2024-02-09 20:45:01 +0300

    Добавляет базовую структуру пакетов

--- app/src/main/java/com/example/kinopoisk_test_app/ui/root/RootActivity.kt
+++ app/src/main/java/com/example/kinopoisk_test_app/ui/root/RootActivity.kt
@@ -1,11 +1,12 @@
-package com.example.kinopoisk_test_app
+package com.example.kinopoisk_test_app.ui.root
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kinopoisk_test_app.R
 
-class MainActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_root)
     }
 }
