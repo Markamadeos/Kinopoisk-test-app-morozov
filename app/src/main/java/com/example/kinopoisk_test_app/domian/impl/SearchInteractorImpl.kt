8f7b74f17531b6474e33ad29d1322a4481883085
Mark Quarasique <julipaha@gmail.com>
2024-02-10 12:00:13 +0300

    Добавляет верстку элемента списка

--- app/src/main/java/com/example/kinopoisk_test_app/domian/impl/SearchInteractorImpl.kt
+++ app/src/main/java/com/example/kinopoisk_test_app/domian/impl/SearchInteractorImpl.kt
@@ -1,12 +1,12 @@
 package com.example.kinopoisk_test_app.domian.impl
 
-import com.example.kinopoisk_test_app.domian.api.SearchInterctor
+import com.example.kinopoisk_test_app.domian.api.SearchInteractor
 import com.example.kinopoisk_test_app.domian.api.SearchRepository
 import com.example.kinopoisk_test_app.domian.models.Movie
 import com.example.kinopoisk_test_app.domian.models.SearchResultData
 import kotlinx.coroutines.flow.Flow
 
-class SearchInteractorImpl(private val searchRepository: SearchRepository) : SearchInterctor {
+class SearchInteractorImpl(private val searchRepository: SearchRepository) : SearchInteractor {
     override suspend fun getPopularMovies(): Flow<SearchResultData<List<Movie>>> {
         return searchRepository.getPopularMovies()
     }
