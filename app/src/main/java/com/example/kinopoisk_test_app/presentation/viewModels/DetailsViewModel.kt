500414a8512b2c914175dbba8e1ad200faa59b3f
Mark Quarasique <julipaha@gmail.com>
2024-02-10 17:39:24 +0300

    Добавляет viewModel Деталей

---
+++ app/src/main/java/com/example/kinopoisk_test_app/presentation/viewModels/DetailsViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.kinopoisk_test_app.presentation.viewModels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kinopoisk_test_app.domian.api.SearchRepository
+import com.example.kinopoisk_test_app.domian.models.Movie
+import com.example.kinopoisk_test_app.domian.models.SearchResultData
+import com.example.kinopoisk_test_app.presentation.models.DetailScreenState
+import kotlinx.coroutines.launch
+
+class DetailsViewModel(private val searchRepository: SearchRepository) : ViewModel() {
+    private val _screenState: MutableLiveData<DetailScreenState> = MutableLiveData()
+    val screenState: LiveData<DetailScreenState> = _screenState
+
+    fun fillData(movieId: String) {
+        _screenState.postValue(DetailScreenState.Loading)
+        viewModelScope.launch {
+            searchRepository.getMovieById(movieId).collect { result ->
+                processingResult(result)
+            }
+        }
+    }
+
+    private fun processingResult(result: SearchResultData<Movie>) {
+        when (result) {
+            is SearchResultData.Empty -> {
+                _screenState.postValue(DetailScreenState.ServerError(result.message))
+            }
+
+            is SearchResultData.Data -> {
+                if (result.value != null) {
+                    _screenState.postValue(DetailScreenState.Content(result.value))
+                } else {
+                    _screenState.postValue(DetailScreenState.Loading)
+                }
+            }
+
+            is SearchResultData.NoInternet -> {
+                _screenState.postValue(DetailScreenState.InternetError(result.message))
+            }
+
+            is SearchResultData.ServerError -> {
+                _screenState.postValue(DetailScreenState.ServerError(result.message))
+            }
+        }
+    }
+
+}
