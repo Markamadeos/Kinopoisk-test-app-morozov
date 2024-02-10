3d2c71310ea8a3b8348fda95451a90c922c38f63
Mark Quarasique <julipaha@gmail.com>
2024-02-10 14:01:17 +0300

    Добавялет новую вью модель в граф зависимостей

--- app/src/main/java/com/example/kinopoisk_test_app/di/ViewModelModule.kt
+++ app/src/main/java/com/example/kinopoisk_test_app/di/ViewModelModule.kt
@@ -1,7 +1,11 @@
 package com.example.kinopoisk_test_app.di
 
+import com.example.kinopoisk_test_app.presentation.viewModels.PopularViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-
+    viewModel {
+        PopularViewModel(searchInteractor = get())
+    }
 }
